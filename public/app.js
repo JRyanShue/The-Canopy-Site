14a9336896e9602d1289dc679a043a1d807f23cd
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-03-30 13:10:12 -0400

    Completed subscription API
    Can register email address under updates page. Notifies user (and doesn't send to database) when email address is not valid (uses regex).

--- public/app.js
+++ public/app.js
@@ -15,15 +15,40 @@ var firestore = firebase.firestore();
 const docRef = firestore.doc("users/names");
 const inputText = document.querySelector("#userEmail");
 const saveButton = document.querySelector("#saveButton");
+const confirmText = document.querySelector("#confirmText");
 
 saveButton.addEventListener("click", function(){
     const textToSave = inputText.value;
-    console.log("SAVING TO FIRESTORE: " + textToSave);
-    docRef.set({
-        name: textToSave
-    }).then(function(){
-        console.log("Name saved!");
-    }).catch(function(error){
-        console.log("Got an error: ", error);
-    })
+
+    //check to see that valid email address is being entered
+    if (validateEmail(textToSave)) {
+        //save email and display success message
+        console.log("SAVING TO FIRESTORE: " + textToSave);
+
+        docRef.set({
+            name: textToSave
+        }).then(function(){
+            console.log("Name saved!");
+            document.getElementById("confirmText").innerHTML=("Thank you for subscribing! You will receive periodic updates about the Canopy.");
+            document.getElementById("confirmText").style.color="white";
+        }).catch(function(error){
+            console.log("Got an error: ", error);
+        })
+    } else {
+        //display fail message
+        document.getElementById("confirmText").innerHTML=("Not a valid email address, please try again.");
+        document.getElementById("confirmText").style.color="red";
+    }
 })
+
+//clear textfield on click
+saveButton.addEventListener("click", () => {
+    inputText.value = "";
+});
+
+//validating email address with regex
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
